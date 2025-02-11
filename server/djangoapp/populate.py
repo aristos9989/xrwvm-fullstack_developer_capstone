30ac84dd4ab2a92393f99c003eef97afe221e1c0
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-02-11 06:38:36 -0500

    Applied Python Linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,8 +15,10 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-            description=data['description']))
+        car_make_instances.append(
+            CarMake.objects.create(name=data['name'],
+            description=data['description'])
+        )
 
     # Create CarModel instances with the corresponding CarMake instances
 
@@ -112,7 +114,6 @@ def initiate():
             'car_make': car_make_instances[4],
             },
         ]
-
 
     for data in car_model_data:
         CarModel.objects.create(name=data['name'],
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -87,8 +87,7 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3',
              'NAME': BASE_DIR / 'db.sqlite3'}}
 
-AUTH_PASSWORD_VALIDATORS = 
-    [
+AUTH_PASSWORD_VALIDATORS = [
         {'NAME': 'django.contrib.auth.password_validation.\
         UserAttributeSimilarityValidator'
         },
